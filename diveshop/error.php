374b68f9cea46618be7ba13f0295e0bd851a6be6
keeis2coo <kjarr88@gmail.com>
2016-01-15 16:13:19 -0600

    Create error.php

---
+++ diveshop/error.php
@@ -0,0 +1,9 @@
+   <div id="page">
+        <div id="main">
+            <h2 class="top">Error</h2>
+            <p><?php echo $error; ?></p>
+        </div>
+    </div><!-- end page -->
+    <p>Click your brower's back button to return to the previous page.</p>
+</body>
+</html>
